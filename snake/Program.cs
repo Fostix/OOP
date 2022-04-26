517d17133ff3cff2167009d6604c56a551aa33d0
Albert <AccretionDiskThat@gmail.com>
2022-04-26 14:09:56 +0500

    hello world

--- snake/Program.cs
+++ snake/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine();
+        }
+    }
+}
